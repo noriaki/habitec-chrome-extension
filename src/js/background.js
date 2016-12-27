a4aa63f65a1fe4e7ca82e01b51f9cd10ed066d81
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2016-12-27 16:44:30 +0900

    update simple messaging

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,22 @@
-async function aaa() { console.log('aaa'); };
+const toggleIcon = (action, sender) => {
+  const method = tabs => chrome.pageAction[action](tabs[0].id);
+  return sender.tab ? method([sender.tab]) : chrome.tabs.query({
+    active: true, currentWindow: true }, method);
+};
 
-(async () => {
-  await aaa();
-  debugger;
-  await aaa();
-})();
+chrome.runtime.onMessage.addListener((action, sender) => {
+  switch (action.type) {
+  case 'icon/show':
+    toggleIcon('show', sender);
+    break;
+  case 'icon/hide':
+    toggleIcon('hide', sender);
+    break;
+  default:
+    break;
+  }
+
+  return true;
+});
+
+chrome.pageAction.onClicked.addListener(tab => console.log(tab));
--- src/js/content_script.js
+++ src/js/content_script.js
@@ -1,14 +1,1 @@
-import K from 'class_test';
-
-const d = { e: 'e', f: 'f' };
-const g = { f: 'fff', h: 'hhh', ...d };
-
-async function bbb() { console.log('bbb'); }
-
-(async () => {
-  await bbb();
-  console.log(g);
-  console.log(new K('hoge'));
-  debugger;
-  await bbb();
-})();
+chrome.runtime.sendMessage({ type: 'icon/show' }, () => {});
