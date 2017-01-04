0df1aaf490744375286a9d4c85b094dc964fb81d
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-01-04 17:25:49 +0900

    update using redux architecture

--- src/js/background.js
+++ src/js/background.js
@@ -1,20 +1,13 @@
-const toggleIcon = (action, sender) => {
-  const method = tabs => chrome.pageAction[action](tabs[0].id);
-  return sender.tab ? method([sender.tab]) : chrome.tabs.query({
-    active: true, currentWindow: true }, method);
-};
+import { createStore } from 'redux';
+import pageIconReducer from 'Reducers/PageIcon';
+import { addMetaToAction, extractTabId } from 'chrome-extension-redux';
+
+const store = createStore(pageIconReducer);
+store.subscribe(() => console.log(store.getState()));
 
 chrome.runtime.onMessage.addListener((action, sender) => {
-  switch (action.type) {
-  case '/page-icon/SHOW':
-    toggleIcon('show', sender);
-    break;
-  case '/page-icon/HIDE':
-    toggleIcon('hide', sender);
-    break;
-  default:
-    break;
-  }
+  const tabId = extractTabId(sender);
+  store.dispatch(addMetaToAction(action, { tabId }));
 
   return true;
 });
